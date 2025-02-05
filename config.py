8d6d505ae600072907f92c9c048ab57fe4c19c29
vash02 <vash02@users.noreply.github.com>
2025-02-05 01:40:37 -0500

    removing values from config file

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 import os
 
 # Environment variable for security
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "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")  # You can set this as an environment variable
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "<YOUR_GIT_TOKEN>")  # You can set this as an environment variable
 LLM_MODEL_NAME = "llama3.2"
 EMBEDDING_MODEL_NAME = "microsoft/codebert-base"
